77af24c3cd4e0ffada9f174d5bd8d5e1d2796ccf
Yusuf <yusuf@madetech.com>
2020-11-30 10:24:18 +0000

    Update application states
    - add Closed - Duplicate state
    - update migration to allow for 10 states
    - move Refer to Manager for coherent application status flow
    
    Co-authored-by: Faissal <faissal@madetech.com>

--- db/migrations/sqls/20201127123942-update-application-states-up.sql
+++ db/migrations/sqls/20201127123942-update-application-states-up.sql
@@ -1,4 +1,4 @@
-DELETE FROM application_state WHERE id > 9;
+DELETE FROM application_state WHERE id > 10;
 
 INSERT INTO application_state(id, description)
 VALUES(1, 'Unprocessed')
@@ -13,43 +13,49 @@ DO UPDATE
 SET description = EXCLUDED.description;
 
 INSERT INTO application_state(id, description)
-VALUES(3, 'Pre-processed Review')
+VALUES(3, 'Pre-processed - Review')
 ON CONFLICT (id)
 DO UPDATE
 SET description = EXCLUDED.description;
 
 INSERT INTO application_state(id, description)
-VALUES(4, 'Refer to Manager')
+VALUES(4, 'Processed - Approved')
 ON CONFLICT (id)
 DO UPDATE
 SET description = EXCLUDED.description;
 
 INSERT INTO application_state(id, description)
-VALUES(5, 'Processed - Approved')
+VALUES(5, 'Processed - Rejected')
 ON CONFLICT (id)
 DO UPDATE
 SET description = EXCLUDED.description;
 
 INSERT INTO application_state(id, description)
-VALUES(6, 'Processed - Rejected')
+VALUES(6, 'NFI')
 ON CONFLICT (id)
 DO UPDATE
 SET description = EXCLUDED.description;
 
 INSERT INTO application_state(id, description)
-VALUES(7, 'NFI')
+VALUES(7, 'Refer to Manager')
 ON CONFLICT (id)
 DO UPDATE
 SET description = EXCLUDED.description;
 
 INSERT INTO application_state(id, description)
-VALUES(8, 'Exported for Payment')
+VALUES(8, 'Closed - Duplicate')
 ON CONFLICT (id)
 DO UPDATE
 SET description = EXCLUDED.description;
 
 INSERT INTO application_state(id, description)
-VALUES(9, 'Declined - Test')
+VALUES(9, 'Exported for Payment')
+ON CONFLICT (id)
+DO UPDATE
+SET description = EXCLUDED.description;
+
+INSERT INTO application_state(id, description)
+VALUES(10, 'Declined - Test')
 ON CONFLICT (id)
 DO UPDATE
 SET description = EXCLUDED.description;
