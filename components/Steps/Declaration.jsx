3cc3096d80c83d43116214573463fad97d435801
mattbee <matt.bee@madetech.com>
2020-11-30 13:34:06 +0000

    Add GDPR data statement to declaration
    As we are collecting personal identifiable information, we need to state
    how we will user people's data. As per the previous grant type, we have
    used the same wording on the declaration page to meet GDPR needs.

--- components/Steps/Declaration.jsx
+++ components/Steps/Declaration.jsx
@@ -135,6 +135,22 @@ const Declaration = (props) => {
           errors
         )}
       />
+      <h2>How we will use your information</h2>
+      <p className="govuk-body">
+        The Council will not accept deliberate manipulation and fraud. Any
+        business caught falsifying their records to gain additional grant money
+        will face prosecution and any funding issued will be subject to
+        clawback. We will use your information to assess your application for
+        financial support. In doing so we will confirm information about you and
+        your account from Council departments and credit reference agencies to
+        confirm account validity and your identity. If you provide false or
+        inaccurate information, we will record this. If you would like full
+        details on how we use your information, please refer to our{' '}
+        <a href="https://hackney.gov.uk/privacy" target="_blank" rel="noopener">
+          privacy policy
+        </a>
+        .
+      </p>
       <Button className="govuk-button" text="Next" type="submit" />
     </form>
   );
